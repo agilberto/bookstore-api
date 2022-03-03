cadd4e43340203428ac4e5ca012e82cad0edf04f
agilberto <agilberto20111@hotmail.com>
2022-03-02 23:00:25 -0300

    Perfil de teste implementado

--- src/main/java/com/gilberto/bookstore/BookstoreApplication.java
+++ src/main/java/com/gilberto/bookstore/BookstoreApplication.java
@@ -1,19 +1,14 @@
 package com.gilberto.bookstore;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.gilberto.bookstore.domain.Categoria;
-import com.gilberto.bookstore.domain.Livro;
 import com.gilberto.bookstore.repositories.CategoriaRepository;
 import com.gilberto.bookstore.repositories.LivroRepository;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication  {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
@@ -24,24 +19,8 @@ public class BookstoreApplication implements CommandLineRunner{
 	
 	public static void main(String[] args) {
 		
-		
-		
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
 	 
-		Categoria cat1 = new Categoria(null, "Informatica", "Livro de TI");
-		
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren Ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
-		
-	}
-
 }
---
+++ src/main/java/com/gilberto/bookstore/service/DBService.java
@@ -0,0 +1,41 @@
+package com.gilberto.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilberto.bookstore.domain.Categoria;
+import com.gilberto.bookstore.domain.Livro;
+import com.gilberto.bookstore.repositories.CategoriaRepository;
+import com.gilberto.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+
+	@Autowired
+	LivroRepository livroRepository;
+	
+	public void instanciaBaseDeDados() {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livro de TI");
+		Categoria cat2 = new Categoria(null, "Ficção Centifica", "Ficção Cientifica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren Ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Robert Martin", "Loren Ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "Robert Martin", "Loren Ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "Robert Martin", "Loren Ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Robert Martin", "Loren Ipsum", cat2);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1,l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+
+}

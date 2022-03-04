b4311af0761c472e81f5452c323b16fded4a64e9
agilberto <agilberto20111@hotmail.com>
2022-03-04 16:50:18 -0300

    finById para categoria

---
+++ src/main/java/com/gilberto/bookstore/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.gilberto.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilberto.bookstore.domain.Categoria;
+import com.gilberto.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}

7e3b14fcb2139f6d09dbe82d39e726eed8119c52
agilberto <agilberto20111@hotmail.com>
2022-03-20 08:01:00 -0300

    DELETE para livro

--- src/main/java/com/gilberto/bookstore/service/LivroService.java
+++ src/main/java/com/gilberto/bookstore/service/LivroService.java
@@ -52,4 +52,10 @@ public class LivroService {
 		 obj.setCategoria(cat);
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		 Livro odj = findById(id);
+		 repository.delete(odj);
+		
+	}
 }
